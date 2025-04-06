605dcde5cb78798dc315a3851004258bfcaa0b96
shakya77 <bijanshakya145@gmail.com>
2025-04-06 22:56:46 +0545

    react router and pages setted up

--- src/Routing/Routing.jsx
+++ src/Routing/Routing.jsx
@@ -1,7 +1,10 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from '../Pages/Home';
+import { useState } from 'react';
 
 export default function Routing() {
+    const [loggedIn, setLoggedIn] = useState(true);
+
     return (
         <Router>
             <Routes>
