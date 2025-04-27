310870c2ec80e7795b7ac0430af7460fa62adaca
shakya77 <bijanshakya145@gmail.com>
2025-04-27 19:22:53 +0545

    url changes on vite

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/Shakya77/School-Student'
+  base: 'https://github.com/Shakya77/School-Student/'
 })
